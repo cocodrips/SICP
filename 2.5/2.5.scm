ebcfc3703002e0065e3f8102bf74f6cda9a72279
cocodrips <cocodrips@gmail.com>
2017-05-24 09:56:07 +0900

    (wip)2.5

--- 2.5/2.5.scm
+++ 2.5/2.5.scm
@@ -27,6 +27,7 @@
 (define (mul x y) (apply-generic 'mul x y))
 (define (div x y) (apply-generic 'div x y))
 
+
 ;; はじめに整数・浮動小数の計算を定義
 (define (install-scheme-number-package)
     (define (tag x) (attach-tag 'scheme-number x))
@@ -38,6 +39,9 @@
         (lambda (x y) (tag (* x y))))
     (put 'div '(scheme-number scheme-number)
         (lambda (x y) (tag (/ x y))))
+    (put 'epq? '(scheme-number scheme-number)
+        (lambda (x y) (= x y))
+    )
     (put 'make 'scheme-number (lambda (x) (tag x)))
 'done)
 
@@ -83,6 +87,11 @@
         (lambda (x y) (tag (mul-rat x y))))
     (put 'div '(rational rational)
         (lambda (x y) (tag (div-rat x y))))
+    (put 'epq? '(rational rational)
+        (lambda (x y) 
+            (and 
+                (= (numer x) (numer y))
+                (= (denom x) (denom y)))))
     (put 'make 'rational
         (lambda (n d) (tag (make-rat n d))))
 'done)
@@ -176,15 +185,16 @@
         (lambda (z1 z2) (tag (mul-complex z1 z2))))
     (put 'div '(complex complex)
         (lambda (z1 z2) (tag (div-complex z1 z2))))
+    (put 'epq? '(complex complex)
+        (lambda (z1 z2) 
+            (and 
+                (= (real-part z1) (real-part z2))
+                (= (imag-part z1) (imag-part z2))
+        )))
     (put 'make-from-real-imag 'complex
         (lambda (x y) (tag (make-from-real-imag x y))))
     (put 'make-from-mag-ang 'complex
         (lambda (r a) (tag (make-from-mag-ang r a))))
-
-    (put 'real-part '(complex) real-part)
-    (put 'imag-part '(complex) imag-part)
-    (put 'magnitude '(complex) magnitude)
-    (put 'angle '(complex) angle)
 'done)
 
 (define (make-complex-from-real-imag x y)
@@ -209,5 +219,99 @@
 (print com)
 
 (print "===Ex2.77===")
-(magnitude com) 
+
+; これはいける
+(define com-mag (make-from-mag-ang 3 4))
+(print (magnitude com-mag))
+
+
+;; 複素数パッケージ
+(define (install-complex-package)
+    ;; 直交形式パッケージと極形式パッケージからインポートした⼿続き
+    (define (make-from-real-imag x y)
+        ((get 'make-from-real-imag 'rectangular) x y))
+    (define (make-from-mag-ang r a)
+        ((get 'make-from-mag-ang 'polar) r a))
+    
+    ;; 内部⼿続き
+    (define (add-complex z1 z2)
+        (make-from-real-imag 
+            (+ (real-part z1) (real-part z2))
+            (+ (imag-part z1) (imag-part z2))))
+    (define (sub-complex z1 z2)
+        (make-from-real-imag 
+            (- (real-part z1) (real-part z2))
+            (- (imag-part z1) (imag-part z2))))
+    (define (mul-complex z1 z2)
+        (make-from-mag-ang 
+            (* (magnitude z1) (magnitude z2))
+            (+ (angle z1) (angle z2))))
+    (define (div-complex z1 z2)
+        (make-from-mag-ang 
+            (/ (magnitude z1) (magnitude z2))
+            (- (angle z1) (angle z2))))
+    ;; システムのほかの部分とのインターフェイス
+    (define (tag z) (attach-tag 'complex z))
+    (put 'add '(complex complex)
+        (lambda (z1 z2) (tag (add-complex z1 z2))))
+    (put 'sub '(complex complex)
+        (lambda (z1 z2) (tag (sub-complex z1 z2))))
+    (put 'mul '(complex complex)
+        (lambda (z1 z2) (tag (mul-complex z1 z2))))
+    (put 'div '(complex complex)        
+        (lambda (z1 z2) (tag (div-complex z1 z2))))
+    (put 'real-part '(complex) real-part)
+    (put 'imag-part '(complex) imag-part)
+    (put 'magnitude '(complex) magnitude)
+    (put 'angle '(complex) angle)
+    (put 'make-from-real-imag 'complex
+        (lambda (x y) (tag (make-from-real-imag x y))))
+    (put 'make-from-mag-ang 'complex
+        (lambda (r a) (tag (make-from-mag-ang r a))))
+'done)
+
+;(print (magnitude com))
+
+(print "===Ex.2.78===")
+
+(define (attach-tag type-tag contents)
+    (if 
+        (number? contents)
+        contents
+        (cons type-tag contents)))
+
+(define (type-tag datum)
+    (cond 
+        ((number? datum) 'scheme-number)
+        ((pair? datum) (car datum))
+    (else 
+        (error "Bad tagged datum: TYPE-TAG" datum))))
+
+(define (contents datum)
+    (cond 
+        ((number? datum) datum)
+        ((pair? datum) (cdr datum))
+    (else
+        (error "Bad tagged datum: CONTENTS" datum))))
+
+
+
+(print "===Ex.2.79===")
+(define (epq? x y) (apply-generic 'epq? x y))
+
+(define num2 (make-scheme-number 5))
+(epq? num num2)
+
+;(define rat2 (make-rational 1 2))
+;(epq? rat rat2)
+
+
+
+
+
+
+
+
+
+
 
