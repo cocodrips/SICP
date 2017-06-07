382e94e8da04969e77a216043664b88be7f845cf
cocodrips <cocodrips@gmail.com>
2017-06-07 09:19:38 +0900

    WIP: 2.5.2

--- 2.5/2.5.scm
+++ 2.5/2.5.scm
@@ -40,204 +40,7 @@
 (define (mul x y) (apply-generic 'mul x y))
 (define (div x y) (apply-generic 'div x y))
 
-
-;; はじめに整数・浮動小数の計算を定義
-(define (install-scheme-number-package)
-    (define (tag x) (attach-tag 'scheme-number x))
-    (put 'add '(scheme-number scheme-number)
-        (lambda (x y) (tag (+ x y))))
-    (put 'sub '(scheme-number scheme-number)
-        (lambda (x y) (tag (- x y))))
-    (put 'mul '(scheme-number scheme-number)
-        (lambda (x y) (tag (* x y))))
-    (put 'div '(scheme-number scheme-number)
-        (lambda (x y) (tag (/ x y))))
-    ; Ex 2.79
-    (put 'epq? '(scheme-number scheme-number) =)
-    ; Ex 2.80
-    (put '=zero? '(scheme-number)
-        (lambda (x) (= x 0)))
-    (put 'make 'scheme-number (lambda (x) (tag x)))
-'done)
-
-(define (make-scheme-number n)
-    ((get 'make 'scheme-number) n))
-
-
-(define (install-rational-package)
-    ;; 内部⼿続き
-    (define (numer x) (car x))
-    (define (denom x) (cdr x))
-    (define (make-rat n d)
-        (let ((g (gcd n d)))
-            (cons (/ n g) (/ d g))))
-    (define (add-rat x y)
-        (make-rat 
-            (+ 
-                (* (numer x) (denom y))
-                (* (numer y) (denom x))
-            )
-            (* (denom x) (denom y))))
-    (define (sub-rat x y)
-        (make-rat 
-            (- (* (numer x) (denom y))
-            (* (numer y) (denom x)))
-            (* (denom x) (denom y))))
-
-    (define (mul-rat x y)
-        (make-rat 
-            (* (numer x) (numer y))
-            (* (denom x) (denom y))))
-    (define (div-rat x y)
-        (make-rat 
-            (* (numer x) (denom y))
-            (* (denom x) (numer y))))
-    ;; インタフェース
-    (define (tag x) (attach-tag 'rational x))
-    (put 'add '(rational rational)
-        (lambda (x y) (tag (add-rat x y))))
-    (put 'sub '(rational rational)
-        (lambda (x y) (tag (sub-rat x y))))
-    (put 'mul '(rational rational)
-        (lambda (x y) (tag (mul-rat x y))))
-    (put 'div '(rational rational)
-        (lambda (x y) (tag (div-rat x y))))
-
-    ; Ex 2.79
-    (put 'epq? '(rational rational) 
-        (lambda (x y) 
-            (= 
-                (* (numer x) (denom y))
-                (* (denom x) (numer y)))))
-    ; Ex 2.80
-    (put '=zero? '(rational)
-        (lambda (x) (= (numer x) 0)))
-    (put 'make 'rational
-        (lambda (n d) (tag (make-rat n d))))
-'done)
-(define (make-rational n d)
-    ((get 'make 'rational) n d))
-
-
-;; 2.4.3より 複素数の各パッケージをinstall
-(define (install-rectangular-package)
-    ;; 内部手続き
-    (define (real-part z) (car z))
-    (define (imag-part z) (cdr z))
-    (define (make-from-real-imag x y) (cons x y))
-    (define (magnitude z)
-        (sqrt (+ 
-            (square (real-part z))
-            (square (imag-part z)))))
-    (define (angle z)
-        (atan (imag-part z) (real-part z)))
-    (define (make-from-mag-ang r a)
-        (cons (* r (cos a)) (* r (sin a))))
-    ;; システムのほかの部分とのインターフェイス
-    (define (tag x) (attach-tag 'rectangular x))
-    (put 'real-part '(rectangular) real-part)
-    (put 'imag-part '(rectangular) imag-part)
-    (put 'magnitude '(rectangular) magnitude)
-    (put 'angle '(rectangular) angle)
-    (put 'make-from-real-imag 'rectangular
-    (lambda (x y) (tag (make-from-real-imag x y))))
-    (put 'make-from-mag-ang 'rectangular
-    (lambda (r a) (tag (make-from-mag-ang r a))))
-'done)
-
-(define (install-polar-package)
-;; 内部手続き
-    (define (magnitude z) (car z))
-    (define (angle z) (cdr z))
-    (define (make-from-mag-ang r a) (cons r a))
-    (define (real-part z) (* (magnitude z) (cos (angle z))))
-    (define (imag-part z) (* (magnitude z) (sin (angle z))))
-    (define (make-from-real-imag x y)
-        (cons 
-            (sqrt (+ (square x) (square y)))
-            (atan y x)))
-
-    ;; システムのほかの部分とのインターフェイス
-    (define (tag x) (attach-tag 'polar x))
-    (put 'real-part '(polar) real-part)
-    (put 'imag-part '(polar) imag-part)
-    (put 'magnitude '(polar) magnitude)
-    (put 'angle '(polar) angle)
-    (put 'make-from-real-imag 'polar
-    (lambda (x y) (tag (make-from-real-imag x y))))
-    (put 'make-from-mag-ang 'polar
-    (lambda (r a) (tag (make-from-mag-ang r a))))
-'done)
-
-
-;; 複素数パッケージ
-(define (install-complex-package)
-    ;; 直交形式パッケージと極形式パッケージからインポートした⼿続き
-    (define (make-from-real-imag x y)
-        ((get 'make-from-real-imag 'rectangular) x y))
-    (define (make-from-mag-ang r a)
-        ((get 'make-from-mag-ang 'polar) r a))
-    
-    ;; 内部⼿続き
-    (define (add-complex z1 z2)
-        (make-from-real-imag 
-            (+ (real-part z1) (real-part z2))
-            (+ (imag-part z1) (imag-part z2))))
-    (define (sub-complex z1 z2)
-        (make-from-real-imag 
-            (- (real-part z1) (real-part z2))
-            (- (imag-part z1) (imag-part z2))))
-    (define (mul-complex z1 z2)
-        (make-from-mag-ang 
-            (* (magnitude z1) (magnitude z2))
-            (+ (angle z1) (angle z2))))
-    (define (div-complex z1 z2)
-        (make-from-mag-ang 
-            (/ (magnitude z1) (magnitude z2))
-            (- (angle z1) (angle z2))))
-    ;; システムのほかの部分とのインターフェイス
-    (define (tag z) (attach-tag 'complex z))
-    (put 'add '(complex complex)
-        (lambda (z1 z2) (tag (add-complex z1 z2))))
-    (put 'sub '(complex complex)
-        (lambda (z1 z2) (tag (sub-complex z1 z2))))
-    (put 'mul '(complex complex)
-        (lambda (z1 z2) (tag (mul-complex z1 z2))))
-    (put 'div '(complex complex)
-        (lambda (z1 z2) (tag (div-complex z1 z2))))
-
-    ; Ex.2.77 
-    ; polar , rectanglarをマップしてcomplexという型にしている
-    ; そのため型に対するwrapperを準備してあげる必要がある
-    (put 'real-part '(complex) real-part)
-    (put 'imag-part '(complex) imag-part)
-    (put 'magnitude '(complex) magnitude)
-    (put 'angle '(complex) angle)
-    
-    ; Ex.2.79
-    (put 'epq? '(complex complex) 
-        (lambda (z1 z2) 
-            (and 
-                (= (real-part z1) (real-part z2))
-                (= (imag-part z1) (imag-part z2))
-        )))
-
-    ; Ex 2.80
-    (put '=zero? '(complex)
-        (lambda (x) (= (real-part x) 0)))
-
-
-    (put 'make-from-real-imag 'complex
-        (lambda (x y) (tag (make-from-real-imag x y))))
-    (put 'make-from-mag-ang 'complex
-        (lambda (r a) (tag (make-from-mag-ang r a))))
-'done)
-
-(define (make-complex-from-real-imag x y)
-    ((get 'make-from-real-imag 'complex) x y))
-(define (make-complex-from-mag-ang r a)
-    ((get 'make-from-mag-ang 'complex) r a))
-
+(load "./disit-packages.scm")
 
 (install-scheme-number-package)
 (install-rational-package)
@@ -256,64 +59,18 @@
 
 (print "===Ex2.77===")
 
-; これはいける
-(define com-mag (make-from-mag-ang 3 4))
-(print com-mag)
+; polar , rectanglarをマップしてcomplexという型にしている
+; そのため型に対するwrapperを準備してあげる必要がある
+(magnitude com) 
 
-;; 複素数パッケージ
-(define (install-complex-package)
-    ;; 直交形式パッケージと極形式パッケージからインポートした⼿続き
-    (define (make-from-real-imag x y)
-        ((get 'make-from-real-imag 'rectangular) x y))
-    (define (make-from-mag-ang r a)
-        ((get 'make-from-mag-ang 'polar) r a))
-    
-    ;; 内部⼿続き
-    (define (add-complex z1 z2)
-        (make-from-real-imag 
-            (+ (real-part z1) (real-part z2))
-            (+ (imag-part z1) (imag-part z2))))
-    (define (sub-complex z1 z2)
-        (make-from-real-imag 
-            (- (real-part z1) (real-part z2))
-            (- (imag-part z1) (imag-part z2))))
-    (define (mul-complex z1 z2)
-        (make-from-mag-ang 
-            (* (magnitude z1) (magnitude z2))
-            (+ (angle z1) (angle z2))))
-    (define (div-complex z1 z2)
-        (make-from-mag-ang 
-            (/ (magnitude z1) (magnitude z2))
-            (- (angle z1) (angle z2))))
-    ;; システムのほかの部分とのインターフェイス
-    (define (tag z) (attach-tag 'complex z))
-    (put 'add '(complex complex)
-        (lambda (z1 z2) (tag (add-complex z1 z2))))
-    (put 'sub '(complex complex)
-        (lambda (z1 z2) (tag (sub-complex z1 z2))))
-    (put 'mul '(complex complex)
-        (lambda (z1 z2) (tag (mul-complex z1 z2))))
-    (put 'div '(complex complex)        
-        (lambda (z1 z2) (tag (div-complex z1 z2))))
 
-    (put 'real-part '(complex) real-part)
-    (put 'imag-part '(complex) imag-part)
-    (put 'magnitude '(complex) magnitude)
-    (put 'angle '(complex) angle)
 
-    (put 'make-from-real-imag 'complex
-        (lambda (x y) (tag (make-from-real-imag x y))))
-    (put 'make-from-mag-ang 'complex
-        (lambda (r a) (tag (make-from-mag-ang r a))))
-'done)
-
-;(print (magnitude com))
 
 (print "===Ex.2.78===")
 
 (define (attach-tag type-tag contents)
     (if 
-        (number? contents)
+        (eq? type-tag 'scheme-number)
         contents
         (cons type-tag contents)))
 
@@ -334,24 +91,26 @@
 
 
 (print "===Ex.2.79===")
-(define (epq? x y) (apply-generic 'epq? x y))
+(define (equ? x y) (apply-generic 'equ? x y))
 
-(print "--number epq?")
+(print "--number equ?") 
 (define num (make-scheme-number 5))
 (define num2 (make-scheme-number 5))
-(print (epq? num num2))
+(print (equ? num num2))
 
-(print "--rational epq?")
+(print "--rational equ?")
 (define rat (make-rational 1 2))
 (define rat2 (make-rational 1 2))
 (define rat3 (make-rational 2 4))
-(print (epq? rat rat2))
-(print (epq? rat rat3))
 
-(print "--complex epq?")
+(print rat3)
+(print (equ? rat rat2))
+(print (equ? rat rat3))
+
+(print "--complex equ?")
 (define com (make-complex-from-mag-ang 3 4))
 (define com2 (make-complex-from-mag-ang 3 4))
-(print (epq? com com2))
+(print (equ? com com2))
 ;できれば直行形式でも検証
 
 ; どちらでも整数値になるような組み合わせがほしい・・・　TODO
@@ -364,21 +123,130 @@
 (define (=zero? x) (apply-generic '=zero? x))
 (define num0 (make-scheme-number 0))
 (define rat0 (make-rational 0 5))
-(define com0 (make-complex-from-mag-ang 0 60))
+(define com0 (make-complex-from-mag-ang 0 0))
 
 (print "zero")
 (print (=zero? num0))
 (print (=zero? rat0))
 (print (=zero? com0))
 
+(define com_non0 (make-complex-from-real-imag 0 10))
 (print "not zero")
 (print (=zero? num))
 (print (=zero? rat))
 (print (=zero? com))
+(print (=zero? com_non0))
+
+
+(print "===2.5.2===")
+
+(load "./../coercion.scm")
+(define (scheme-number->complex n)
+    (make-complex-from-real-imag (contents n) 0))
+
+(put-coercion 'scheme-number 'complex scheme-number->complex)
+
+(define (apply-generic op . args)
+    (print "@@@@@@@@@@@@use apply-generic")
+    (let ((type-tags (map type-tag args)))
+        (let ((proc (get op type-tags)))
+            (if proc
+                (apply proc (map contents args))
+
+                #?=(if (= (length args) 2)
+                    (let 
+                        (
+                            (type1 (car type-tags))
+                            (type2 (cadr type-tags))
+                            (a1 (car args))
+                            (a2 (cadr args))
+                        )
+                        (let 
+                            (
+                                #?=(t1->t2 (get-coercion type1 type2))
+                                #?=(t2->t1 (get-coercion type2 type1))
+                            )
+                            (cond 
+                                (t1->t2 (apply-generic op (t1->t2 a1) a2))
+                                (t2->t1 (apply-generic op a1 (t2->t1 a2)))
+                            (else (error "No method for these types"
+                                (list op type-tags ))))))
+                            (error "No method for these types"
+                                        (list op type-tags )))))))
+
+
+;apply-generic ⼿続きは、次のように設計し直すことができます。
+;それぞ れの型に対して、その型のオブジェクトをタワーの⼀階上に “上げる”raise という⼿続きを⽤意します。
+;こうすると、システムが異なる型のオブジェクトの演算を⾏うよう求められたとき、
+;すべてのオブジェクトが塔の同じ階に揃うようになるまで低い型を連続して上げていくということができるようになります
+
+
+(print "===Ex2.81===")
+;Louis Reasoner は、引数の型がすでに同じであっても、
+;apply-generic は引数をお互いの型に強制型変換しようとしてもいいのではないかと気がついた。
+;そのため、それぞれの型の引数をそれ⾃⾝の型に強制型変換 (coerce) する⼿続きを強制型変換テーブルに⼊れる必要があると彼は考えた。
+;例えば、上に⽰した scheme-number->complex という強制型変換に加え、彼は次のことを⾏う。
+
+(define (scheme-number->scheme-number n) n)
+(define (complex->complex z) z)
+(put-coercion 
+    'scheme-number
+    'scheme-number
+    scheme-number->scheme-number)
+(put-coercion 'complex 'complex complex->complex)
+
+
+(define (exp x y) (apply-generic 'exp x y))
+
+(print "---a")
+(define s1 (make-scheme-number 5))
+(define s2 (make-scheme-number 2))
+(print (exp s1 s2)) ;25
+
+
+(define c1 (make-complex-from-real-imag 5 1))
+(define c2 (make-complex-from-real-imag 2 1))
+;(print (exp c1 c2)) 
+;*** ERROR: no such object: GET: (exp complex complex)
+; うーーーん
+
+
+(print "---b")
+(define r1 (make-rational 2 3))
+(print (add s1 c1))
+;*** ERROR: no such object: GET: (add scheme-number complex)
+; それはそう。
+; なぜうごかない？？？
 
 
 
+(define (apply-generic op . args)
+    (print "@@@@@@@@@@@@use apply-generic")
+    (let ((type-tags (map type-tag args)))
+        (let ((proc (get op type-tags)))
+            (if proc
+                (apply proc (map contents args))
 
+                #?=(if (= (length args) 2)
+                    (let 
+                        (
+                            (type1 (car type-tags))
+                            (type2 (cadr type-tags))
+                            (a1 (car args))
+                            (a2 (cadr args))
+                        )
+                        (let 
+                            (
+                                #?=(t1->t2 (get-coercion type1 type2))
+                                #?=(t2->t1 (get-coercion type2 type1))
+                            )
+                            (cond 
+                                (t1->t2 (apply-generic op (t1->t2 a1) a2))
+                                (t2->t1 (apply-generic op a1 (t2->t1 a2)))
+                            (else (error "No method for these types"
+                                (list op type-tags ))))))
+                            (error "No method for these types"
+                                        (list op type-tags )))))))
 
 
 
---
+++ 2.5/disit-packages.scm
@@ -0,0 +1,214 @@
+
+;; はじめに整数・浮動小数の計算を定義
+(define (install-scheme-number-package)
+    (define (tag x) (attach-tag 'scheme-number x))
+    (put 'add '(scheme-number scheme-number)
+        (lambda (x y) (tag (+ x y))))
+    (put 'sub '(scheme-number scheme-number)
+        (lambda (x y) (tag (- x y))))
+    (put 'mul '(scheme-number scheme-number)
+        (lambda (x y) (tag (* x y))))
+    (put 'div '(scheme-number scheme-number)
+        (lambda (x y) (tag (/ x y))))
+
+    ;;;;;;;;;;;;; Ex 2.79
+    (put 'equ? '(scheme-number scheme-number) =)
+
+    ;;;;;;;;;;;;; Ex 2.80
+    (put '=zero? '(scheme-number)
+        (lambda (x) (= x 0)))
+    
+    ;;;;;;;;;;;;; Ex 2.81
+    (put 'exp '(scheme-number scheme-number)
+        (lambda (x y) (tag (expt x y)))) 
+
+
+    (put 'make 'scheme-number (lambda (x) (tag x)))
+'done)
+
+(define (make-scheme-number n)
+    ((get 'make 'scheme-number) n))
+
+
+(define (install-rational-package)
+    ;; 内部⼿続き
+    (define (numer x) (car x))
+    (define (denom x) (cdr x))
+    (define (make-rat n d)
+        (let ((g (gcd n d)))
+            (cons (/ n g) (/ d g))))
+    (define (add-rat x y)
+        (make-rat 
+            (+ 
+                (* (numer x) (denom y))
+                (* (numer y) (denom x))
+            )
+            (* (denom x) (denom y))))
+    (define (sub-rat x y)
+        (make-rat 
+            (- (* (numer x) (denom y))
+            (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+    (define (mul-rat x y)
+        (make-rat 
+            (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+    (define (div-rat x y)
+        (make-rat 
+            (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+    ;; インタフェース
+    (define (tag x) (attach-tag 'rational x))
+    (put 'add '(rational rational)
+        (lambda (x y) (tag (add-rat x y))))
+    (put 'sub '(rational rational)
+        (lambda (x y) (tag (sub-rat x y))))
+    (put 'mul '(rational rational)
+        (lambda (x y) (tag (mul-rat x y))))
+    (put 'div '(rational rational)
+        (lambda (x y) (tag (div-rat x y))))
+
+    ;;;;;;;;;;;;; Ex 2.79 
+    (put 'equ? '(rational rational) 
+        (lambda (x y) 
+            (= 
+                (* (numer x) (denom y))
+                (* (denom x) (numer y)))))
+    ;;;;;;;;;;;;; Ex 2.80
+    (put '=zero? '(rational)
+        (lambda (x) (= (numer x) 0)))
+
+    (put 'make 'rational
+        (lambda (n d) (tag (make-rat n d))))
+'done)
+(define (make-rational n d)
+    ((get 'make 'rational) n d))
+
+
+;; 2.4.3より 複素数の各パッケージをinstall
+(define (install-rectangular-package)
+    ;; 内部手続き
+    (define (real-part z) (car z))
+    (define (imag-part z) (cdr z))
+    (define (make-from-real-imag x y) (cons x y))
+    (define (magnitude z)
+        (sqrt (+ 
+            (square (real-part z))
+            (square (imag-part z)))))
+    (define (angle z)
+        (atan (imag-part z) (real-part z)))
+    (define (make-from-mag-ang r a)
+        (cons (* r (cos a)) (* r (sin a))))
+    ;; システムのほかの部分とのインターフェイス
+    (define (tag x) (attach-tag 'rectangular x))
+    (put 'real-part '(rectangular) real-part)
+    (put 'imag-part '(rectangular) imag-part)
+    (put 'magnitude '(rectangular) magnitude)
+    (put 'angle '(rectangular) angle)
+    (put 'make-from-real-imag 'rectangular
+    (lambda (x y) (tag (make-from-real-imag x y))))
+    (put 'make-from-mag-ang 'rectangular
+    (lambda (r a) (tag (make-from-mag-ang r a))))
+'done)
+
+(define (install-polar-package)
+;; 内部手続き
+    (define (magnitude z) (car z))
+    (define (angle z) (cdr z))
+    (define (make-from-mag-ang r a) (cons r a))
+    (define (real-part z) (* (magnitude z) (cos (angle z))))
+    (define (imag-part z) (* (magnitude z) (sin (angle z))))
+    (define (make-from-real-imag x y)
+        (cons 
+            (sqrt (+ (square x) (square y)))
+            (atan y x)))
+
+    ;; システムのほかの部分とのインターフェイス
+    (define (tag x) (attach-tag 'polar x))
+    (put 'real-part '(polar) real-part)
+    (put 'imag-part '(polar) imag-part)
+    (put 'magnitude '(polar) magnitude)
+    (put 'angle '(polar) angle)
+    (put 'make-from-real-imag 'polar
+    (lambda (x y) (tag (make-from-real-imag x y))))
+    (put 'make-from-mag-ang 'polar
+    (lambda (r a) (tag (make-from-mag-ang r a))))
+'done)
+
+
+;; 複素数パッケージ
+(define (install-complex-package)
+    ;; 直交形式パッケージと極形式パッケージからインポートした⼿続き
+    (define (make-from-real-imag x y)
+        ((get 'make-from-real-imag 'rectangular) x y))
+    (define (make-from-mag-ang r a)
+        ((get 'make-from-mag-ang 'polar) r a))
+    
+    ;; 内部⼿続き
+    (define (add-complex z1 z2)
+        (make-from-real-imag 
+            (+ (real-part z1) (real-part z2))
+            (+ (imag-part z1) (imag-part z2))))
+    (define (sub-complex z1 z2)
+        (make-from-real-imag 
+            (- (real-part z1) (real-part z2))
+            (- (imag-part z1) (imag-part z2))))
+    (define (mul-complex z1 z2)
+        (make-from-mag-ang 
+            (* (magnitude z1) (magnitude z2))
+            (+ (angle z1) (angle z2))))
+    (define (div-complex z1 z2)
+        (make-from-mag-ang 
+            (/ (magnitude z1) (magnitude z2))
+            (- (angle z1) (angle z2))))
+    ;; システムのほかの部分とのインターフェイス
+    (define (tag z) (attach-tag 'complex z))
+    (put 'add '(complex complex)
+        (lambda (z1 z2) (tag (add-complex z1 z2))))
+    (put 'sub '(complex complex)
+        (lambda (z1 z2) (tag (sub-complex z1 z2))))
+    (put 'mul '(complex complex)
+        (lambda (z1 z2) (tag (mul-complex z1 z2))))
+    (put 'div '(complex complex)
+        (lambda (z1 z2) (tag (div-complex z1 z2))))
+
+    ;;;;;;;;;;;;; Ex 2.77
+    ; polar , rectanglarをマップしてcomplexという型にしている
+    ; そのため型に対するwrapperを準備してあげる必要がある
+    (put 'real-part '(complex) real-part)
+    (put 'imag-part '(complex) imag-part)
+    (put 'magnitude '(complex) magnitude)
+    (put 'angle '(complex) angle)
+    
+    ;;;;;;;;;;;;; Ex 2.79
+    (put 'equ? '(complex complex) 
+        (lambda (z1 z2) 
+            (and 
+                (= (real-part z1) (real-part z2))
+                (= (imag-part z1) (imag-part z2))
+        )))
+
+    ;;;;;;;;;;;;; Ex 2.80
+    (put '=zero? '(complex)
+        (lambda (x) 
+            (and 
+                (= (real-part x) 0) 
+                (= (imag-part x) 0)) ))
+
+    ;; こっちでもいい
+    ;(put '=zero? '(complex)
+    ;(lambda (x) 
+    ;    (= (magnitude x) 0)
+
+
+    (put 'make-from-real-imag 'complex
+        (lambda (x y) (tag (make-from-real-imag x y))))
+    (put 'make-from-mag-ang 'complex
+        (lambda (r a) (tag (make-from-mag-ang r a))))
+'done)
+
+(define (make-complex-from-real-imag x y)
+    ((get 'make-from-real-imag 'complex) x y))
+(define (make-complex-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'complex) r a))
