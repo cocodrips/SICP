c1bc92884eff1ee29cf1034d85da5a837a991b1d
cocodrips <cocodrips@gmail.com>
2017-06-26 13:34:01 +0900

    [WIP] 2.88

--- 2.5/2.5.scm
+++ 2.5/2.5.scm
@@ -544,6 +544,74 @@
 
 
 
+(print "====================")
+(print "       2.5.3")
+(print "====================")
+
+; poly: 多項式
+(define (add-poly p1 p2)
+    (if 
+        (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+            (add-terms (term-list p1) (term-list p2)))
+        (error "Polys not in same var: ADD-POLY" (list p1 p2))))
+
+(define (mul-poly p1 p2)
+    (if 
+        (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+            (mul-terms (term-list p1) (term-list p2)))
+        (error "Polys not in same var: MUL-POLY" (list p1 p2))))
+
+
+(define (install-polynomial-package)
+    ;; 内部⼿続き
+    ;; poly の表現
+    (define (make-poly variable term-list) (cons variable term-list))
+    (define (variable p) (car p))
+    (define (term-list p) (cdr p))
+    (define (zero? p) (= (variable p) 0))
+    
+
+    ;⟨2.3.2 節の same-variable? と variable? ⼿続き⟩
+    ;; 項と項リストの表現
+    ;⟨下記の adjoin-term . . . coeff ⼿続き⟩
+    (define (add-poly p1 p2) p1)
+    ;    ⟨add-poly が使う⼿続き⟩
+    (define (mul-poly p1 p2) p1)
+    ;   ⟨mul-poly が使う⼿続き⟩
+
+    ;; システムのほかの部分とのインターフェイス
+    (define (tag p) (attach-tag 'polynomial p))
+    (put 'add '(polynomial polynomial)
+        (lambda (p1 p2) (tag (add-poly p1 p2))))
+    (put 'mul '(polynomial polynomial)
+        (lambda (p1 p2) (tag (mul-poly p1 p2))))
+    (put '=zero? '(polynomial) zero?)
+
+    (put 'make 'polynomial
+        (lambda (var terms) (tag (make-poly var terms))))
+'done)
+
+(define (make-polynomial var terms)
+    ((get 'make 'polynomial) var terms))
+
+(install-polynomial-package)
+
+(print "===Ex2.87===")
+(define p0 (make-polynomial 0 'x))
+(define p1 (make-polynomial 3 'x))
+(define p2 (make-polynomial 5 'x))
+
+(define (ex-2-87)
+    (print "p0: " p0 " zero?="(=zero? p0))
+    (print "p1: " p1 " zero?="(=zero? p1))
+)
+(ex-2-87)
+
+
+(print "===Ex2.88===")
+(print (add p1 p2))
 
 
 
@@ -552,9 +620,59 @@
 
 
 
+(define (add-terms L1 L2)
+    (cond 
+        ((empty-termlist? L1) L2)
+        ((empty-termlist? L2) L1)
+    (else
+        (let 
+            (
+                (t1 (first-term L1))
+                (t2 (first-term L2))
+            )
+            (cond 
+                ((> (order t1) (order t2))
+                    (adjoin-term　t1 (add-terms (rest-terms L1) L2)))
+                ((< (order t1) (order t2))
+                    (adjoin-term　t2 (add-terms L1 (rest-terms L2))))
+            (else
+                (adjoin-term　(make-term (order t1)
+                (add (coeff t1) (coeff t2)))
+                (add-terms (rest-terms L1)
+                (rest-terms L2))))))
+    )))
+
+(define (mul-terms L1 L2)
+    (if 
+        (empty-termlist? L1)
+        (the-empty-termlist)
+        (add-terms (mul-term-by-all-terms (first-term L1) L2)
+            (mul-terms (rest-terms L1) L2))))
+
+(define (mul-term-by-all-terms t1 L)
+    (if 
+        (empty-termlist? L)
+        (the-empty-termlist)
+        (let 
+            ((t2 (first-term L)))
+            (adjoin-term
+                (make-term (+ (order t1) (order t2))
+                (mul (coeff t1) (coeff t2)))
+                (mul-term-by-all-terms t1 (rest-terms L))))))
 
 
-
+(define (adjoin-term term term-list)
+    (if 
+        (=zero? (coeff term))
+        term-list
+        (cons term term-list)))
+(define (the-empty-termlist) '())
+(define (first-term term-list) (car term-list))
+(define (rest-terms term-list) (cdr term-list))
+(define (empty-termlist? term-list) (null? term-list))
+(define (make-term order coeff) (list order coeff))
+(define (order term) (car term))
+(define (coeff term) (cadr term))
 
 
 
