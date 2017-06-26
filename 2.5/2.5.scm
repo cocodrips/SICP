62eea28d5418de892e5c3f5558d1ac19541655d9
cocodrips <cocodrips@gmail.com>
2017-06-26 21:40:57 +0900

    ~Ex2.88

--- 2.5/2.5.scm
+++ 2.5/2.5.scm
@@ -549,20 +549,9 @@
 (print "====================")
 
 ; poly: 多項式
-(define (add-poly p1 p2)
-    (if 
-        (same-variable? (variable p1) (variable p2))
-        (make-poly (variable p1)
-            (add-terms (term-list p1) (term-list p2)))
-        (error "Polys not in same var: ADD-POLY" (list p1 p2))))
 
-(define (mul-poly p1 p2)
-    (if 
-        (same-variable? (variable p1) (variable p2))
-        (make-poly (variable p1)
-            (mul-terms (term-list p1) (term-list p2)))
-        (error "Polys not in same var: MUL-POLY" (list p1 p2))))
 
+(use srfi-1) ;filter-mapの為
 
 (define (install-polynomial-package)
     ;; 内部⼿続き
@@ -570,24 +559,112 @@
     (define (make-poly variable term-list) (cons variable term-list))
     (define (variable p) (car p))
     (define (term-list p) (cdr p))
-    (define (zero? p) (= (variable p) 0))
-    
 
     ;⟨2.3.2 節の same-variable? と variable? ⼿続き⟩
+    (define (variable? x) (symbol? x))
+    (define (same-variable? v1 v2)
+        (and 
+            (variable? v1)
+            (variable? v2)
+            (eq? v1 v2)))
     ;; 項と項リストの表現
-    ;⟨下記の adjoin-term . . . coeff ⼿続き⟩
-    (define (add-poly p1 p2) p1)
-    ;    ⟨add-poly が使う⼿続き⟩
-    (define (mul-poly p1 p2) p1)
-    ;   ⟨mul-poly が使う⼿続き⟩
+    ;⟨下記の adjoin-term . . . coeff ⼿続き
+    (define (adjoin-term term term-list)
+        (if 
+            (=zero? (coeff term))
+            term-list
+            (cons term term-list)))
+    (define (the-empty-termlist) '())
+    (define (first-term term-list) (car term-list))
+    (define (rest-terms term-list) (cdr term-list))
+    (define (empty-termlist? term-list) (null? term-list))
+    (define (make-term order coeff) (list order coeff))
+    (define (order term) (car term))
+    (define (coeff term) (cadr term))
+
+    (define (add-terms L1 L2)
+        (cond 
+            ((empty-termlist? L1) L2)
+            ((empty-termlist? L2) L1)
+        (else
+            (let 
+                (
+                    (t1 (first-term L1))
+                    (t2 (first-term L2))
+                )
+                (cond 
+                    ((> (order t1) (order t2))
+                        (adjoin-term　t1 (add-terms (rest-terms L1) L2)))
+                    ((< (order t1) (order t2))
+                        (adjoin-term　t2 (add-terms L1 (rest-terms L2))))
+                (else
+                    (adjoin-term　(make-term (order t1)
+                    (add (coeff t1) (coeff t2)))
+                    (add-terms (rest-terms L1)
+                    (rest-terms L2))))))
+        )))
+
+    (define (mul-terms L1 L2)
+        (if 
+            (empty-termlist? L1)
+            (the-empty-termlist)
+            (add-terms (mul-term-by-all-terms (first-term L1) L2)
+                (mul-terms (rest-terms L1) L2))))
+
+    (define (mul-term-by-all-terms t1 L)
+        (if 
+            (empty-termlist? L)
+            (the-empty-termlist)
+            (let 
+                ((t2 (first-term L)))
+                (adjoin-term
+                    (make-term (+ (order t1) (order t2))
+                    (mul (coeff t1) (coeff t2)))
+                    (mul-term-by-all-terms t1 (rest-terms L))))))
+
+
+    (define (add-poly p1 p2) 
+        (if 
+            (same-variable? (variable p1) (variable p2))
+            (make-poly (variable p1)
+                (add-terms (term-list p1) (term-list p2)))
+            (error "Polys not in same var: ADD-POLY" (list p1 p2))))
+
+    (define (mul-poly p1 p2)
+        (if 
+            (same-variable? (variable p1) (variable p2))
+            (make-poly (variable p1)
+                (mul-terms (term-list p1) (term-list p2)))
+            (error "Polys not in same var: MUL-POLY" (list p1 p2))))
+
+    ; Ex 2.87
+    (define (=zero-poly? p) 
+        (= 0 (fold-left + 0 (map coeff (term-list p))))
+    )
+
+    (define (negative p)
+        (make-poly 
+            (variable p) 
+            (map list
+                (map order (term-list p))
+                (map (lambda (x) (* -1 (coeff x))) (term-list p))))
+    )
 
     ;; システムのほかの部分とのインターフェイス
     (define (tag p) (attach-tag 'polynomial p))
     (put 'add '(polynomial polynomial)
         (lambda (p1 p2) (tag (add-poly p1 p2))))
+
+    (put 'sub '(polynomial polynomial) 
+        (lambda (p1 p2) (tag (add-poly p1 (negative p2)))))
+
+    ;(put 'sub '(polynomial polynomial) 
+    ;    (lambda (p1 p2) (negative p2)))
+
     (put 'mul '(polynomial polynomial)
         (lambda (p1 p2) (tag (mul-poly p1 p2))))
-    (put '=zero? '(polynomial) zero?)
+    
+    (put '=zero? '(polynomial) =zero-poly?)
 
     (put 'make 'polynomial
         (lambda (var terms) (tag (make-poly var terms))))
@@ -596,92 +673,239 @@
 (define (make-polynomial var terms)
     ((get 'make 'polynomial) var terms))
 
+
 (install-polynomial-package)
 
+(define (apply-generic op . args)
+    (let 
+        ((type-tags (map type-tag args)))
+            (let ((proc (get op type-tags)))
+                (if proc
+                    (apply proc (map contents args))
+                (error
+                    "No method for these types: APPLY-GENERIC"
+                    (list op type-tags ))))))
+
+
+
 (print "===Ex2.87===")
-(define p0 (make-polynomial 0 'x))
-(define p1 (make-polynomial 3 'x))
-(define p2 (make-polynomial 5 'x))
+;2x + 1
+(define p0 (make-polynomial 'x (list (list 1 2) (list 0 1))))
+;2x**2
+(define p1 (make-polynomial 'x (list (list 2 2))))
+; 0
+(define p2 (make-polynomial 'x (list (list 0 0))))
+(define p3 (make-polynomial 'x (list (list 1 0) (list 0 0))))
+
+; Ex 2.87
+
+(define (=zero-poly? p) 
+    (= 0 (fold-left + 0 (map coeff (term-list p))))
+)
 
 (define (ex-2-87)
-    (print "p0: " p0 " zero?="(=zero? p0))
-    (print "p1: " p1 " zero?="(=zero? p1))
+    (print "p0: 2x + 1 = " p0)
+    (print "p1: 2x ** 2 = " p1)
+    (print "p2: 0 = " p2)
+    (print "p3: 0x + 0 = " p3)
+
+    (print (contents p0))
+    (print (map car (cdr (contents p0))))
+    (print (=zero? p0))
+    (print (=zero? p1))
+    (print (=zero? p2))
+    (print (=zero? p3))
+
+    ;p0: 2x + 1 = (polynomial x (1 2) (0 1))
+    ;p1: 2x ** 2 = (polynomial x (2 2))
+    ;p2: 0 = (polynomial x (0 0))
+    ;p3: 0x + 0 = (polynomial x (1 0) (0 0))
+    ;(x (1 2) (0 1))
+    ;(1 0)
+    ;#f
+    ;#f
+    ;#t
+    ;#t
+
 )
 (ex-2-87)
 
 
+
 (print "===Ex2.88===")
-(print (add p1 p2))
+(define (ex-2-87)
+    (print "p0: 2x + 1 = " p0)
+    (print "p1: 2x ** 2 = " p1)
+    (print "p2: 0 = " p2)
+    (print "p3: 0x + 0 = " p3)
+
+    (print "p0 + p1 = " (add p0 p1))
+    (print "p0 - p1 = " (sub p0 p1))
+
+    ;p0: 2x + 1 = (polynomial x (1 2) (0 1))
+    ;p1: 2x ** 2 = (polynomial x (2 2))
+    ;p2: 0 = (polynomial x (0 0))
+    ;p3: 0x + 0 = (polynomial x (1 0) (0 0))
+    ;p0 + p1 = (polynomial x (2 2) (1 2) (0 1))
+    ;p0 - p1 = (polynomial x (2 -2) (1 2) (0 1))
+)
+(ex-2-87)
+
+
+(print "===Ex2.89===")
+(define (install-polynomial-package)
+    ;; 内部⼿続き
+    ;; poly の表現
+    (define (make-poly variable coeff-list) (cons variable coeff-list))
+    (define (variable p) (car p))
+    (define (coeff-list p) (cdr p))
+
+    ;⟨2.3.2 節の same-variable? と variable? ⼿続き⟩
+    (define (variable? x) (symbol? x))
+    (define (same-variable? v1 v2)
+        (and 
+            (variable? v1)
+            (variable? v2)
+            (eq? v1 v2)))
+    ;; 項と項リストの表現
+    ;⟨下記の adjoin-term . . . coeff ⼿続き
+    (define (the-empty-termlist) '())
+    (define (first-term term-list) (car term-list))
+    (define (rest-terms term-list) (cdr term-list))
+    (define (empty-termlist? term-list) (null? term-list))
+    (define (make-term order coeff) (list order coeff))
+    (define (order term) (car term))
+    (define (coeff term) (cadr term))
+
+    (define (add-terms L1 L2)
+        (cond 
+            ((empty-termlist? L1) L2)
+            ((empty-termlist? L2) L1)
+        (else
+            (let 
+                (
+                    (t1 (first-term L1))
+                    (t2 (first-term L2))
+                )
+                (cond 
+                    ((> (order t1) (order t2))
+                        (adjoin-term　t1 (add-terms (rest-terms L1) L2)))
+                    ((< (order t1) (order t2))
+                        (adjoin-term　t2 (add-terms L1 (rest-terms L2))))
+                (else
+                    (adjoin-term　(make-term (order t1)
+                    (add (coeff t1) (coeff t2)))
+                    (add-terms (rest-terms L1)
+                    (rest-terms L2))))))
+        )))
+
+    (define (mul-terms L1 L2)
+        (if 
+            (empty-termlist? L1)
+            (the-empty-termlist)
+            (add-terms (mul-term-by-all-terms (first-term L1) L2)
+                (mul-terms (rest-terms L1) L2))))
+
+    (define (mul-term-by-all-terms t1 L)
+        (if 
+            (empty-termlist? L)
+            (the-empty-termlist)
+            (let 
+                ((t2 (first-term L)))
+                (adjoin-term
+                    (make-term (+ (order t1) (order t2))
+                    (mul (coeff t1) (coeff t2)))
+                    (mul-term-by-all-terms t1 (rest-terms L))))))
+
+
+    (define (add-poly p1 p2) 
+        (if 
+            #?=(same-variable? (variable p1) (variable p2))
+            (make-poly (variable p1)
+                (map + (coeff-list p1) (coeff-list p2)))) ;<== mapの長さ揃うやつさがす
+            (error "Polys not in same var: ADD-POLY" (list p1 p2) (coeff-list p1) (coeff-list p2)))
+
+    ;(define (mul-poly p1 p2)
+    ;    (if 
+    ;        (same-variable? (variable p1) (variable p2))
+    ;        (make-poly (variable p1)
+    ;            (mul-terms (term-list p1) (term-list p2)))
+    ;        (error "Polys not in same var: MUL-POLY" (list p1 p2))))
+
+    ; Ex 2.87
+    (define (=zero-poly? p) 
+        (= 0 (fold-left + 0 (map coeff (term-list p))))
+    )
+
+    (define (negative p)
+        (make-poly 
+            (variable p) 
+            (map (lambda (x) (* -1 x)) (coeff-list p)))
+    )
+
+    ;; システムのほかの部分とのインターフェイス
+    (define (tag p) (attach-tag 'polynomial p))
+
+    (put 'add '(polynomial polynomial)
+        (lambda (p1 p2) (tag (add-poly p1 p2))))
+
+    (put 'sub '(polynomial polynomial) 
+        (lambda (p1 p2) (tag (add-poly p1 (negative p2)))))
+
+
+    (put 'mul '(polynomial polynomial)
+        (lambda (p1 p2) (tag (mul-poly p1 p2))))
+    
+    (put '=zero? '(polynomial) =zero-poly?)
+
+    (put 'make 'polynomial
+        (lambda (var terms) (tag (make-poly var terms))))
+'done)
+
+(define (make-polynomial var terms)
+    ((get 'make 'polynomial) var terms))
+
+
+(install-polynomial-package)
+
+(define (apply-generic op . args)
+    (let 
+        ((type-tags (map type-tag args)))
+            (let ((proc (get op type-tags)))
+                (if proc
+                    (apply proc (map contents args))
+                (error
+                    "No method for these types: APPLY-GENERIC"
+                    (list op type-tags ))))))
 
 
 
+(install-polynomial-package)
+;2x + 1
+(define p0 (make-polynomial 'x (list 1 2)))
+;2x**2
+(define p1 (make-polynomial 'x (list 0 0 2)))
+; 0
+(define p2 (make-polynomial 'x (list 0)))
+(define p3 (make-polynomial 'x (list 0 0)))
 
+(define (ex-2-89)
+    (print "p0: 2x + 1 = " p0)
+    (print "p1: 2x ** 2 = " p1)
+    (print "p2: 0 = " p2)
+    (print "p3: 0x + 0 = " p3)
 
+    (print "p0 + p1 = " (add p0 p1))
+    (print "p0 - p1 = " (sub p0 p1))
 
-
-
-(define (add-terms L1 L2)
-    (cond 
-        ((empty-termlist? L1) L2)
-        ((empty-termlist? L2) L1)
-    (else
-        (let 
-            (
-                (t1 (first-term L1))
-                (t2 (first-term L2))
-            )
-            (cond 
-                ((> (order t1) (order t2))
-                    (adjoin-term　t1 (add-terms (rest-terms L1) L2)))
-                ((< (order t1) (order t2))
-                    (adjoin-term　t2 (add-terms L1 (rest-terms L2))))
-            (else
-                (adjoin-term　(make-term (order t1)
-                (add (coeff t1) (coeff t2)))
-                (add-terms (rest-terms L1)
-                (rest-terms L2))))))
-    )))
-
-(define (mul-terms L1 L2)
-    (if 
-        (empty-termlist? L1)
-        (the-empty-termlist)
-        (add-terms (mul-term-by-all-terms (first-term L1) L2)
-            (mul-terms (rest-terms L1) L2))))
-
-(define (mul-term-by-all-terms t1 L)
-    (if 
-        (empty-termlist? L)
-        (the-empty-termlist)
-        (let 
-            ((t2 (first-term L)))
-            (adjoin-term
-                (make-term (+ (order t1) (order t2))
-                (mul (coeff t1) (coeff t2)))
-                (mul-term-by-all-terms t1 (rest-terms L))))))
-
-
-(define (adjoin-term term term-list)
-    (if 
-        (=zero? (coeff term))
-        term-list
-        (cons term term-list)))
-(define (the-empty-termlist) '())
-(define (first-term term-list) (car term-list))
-(define (rest-terms term-list) (cdr term-list))
-(define (empty-termlist? term-list) (null? term-list))
-(define (make-term order coeff) (list order coeff))
-(define (order term) (car term))
-(define (coeff term) (cadr term))
-
-
-
-
-
-
-
-
-
+    ;p0: 2x + 1 = (polynomial x (1 2) (0 1))
+    ;p1: 2x ** 2 = (polynomial x (2 2))
+    ;p2: 0 = (polynomial x (0 0))
+    ;p2: 0x + 0 = (polynomial x (1 0) (0 0))
+    ;p0 + p1 = (polynomial x (2 2) (1 2) (0 1))
+    ;p0 - p1 = (polynomial x (2 -2) (1 2) (0 1))
+)
+(ex-2-87)
 
 
 
